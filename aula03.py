9cf4dad24fdfa9811cf3664ff410d50275614c57
Clayton-Dias <crad13@hotmail.com>
2024-07-03 16:54:35 -0300

    Create aula03.py

---
+++ aula03.py
@@ -0,0 +1,12 @@
+minha_var = "No meu estado \"Biscoito\"não é bolacha"
+print("minha_var")
+
+#if minha_var == "Valor":
+#    print("Igual")
+#else:
+#    print ("Diferente")
+
+"""
+Teste teste
+"""
+
